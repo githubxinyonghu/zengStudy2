5a0a140f227074dd217b5ada6f211d59e2589bc0
zengStudy <1650504678@qq.com>
2023-05-17 14:21:01 +0800

    新建了springboot

---
+++ gitspringboot/src/main/java/com/zengstudy/GitspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.zengstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitspringbootApplication.class, args);
+    }
+
+}
